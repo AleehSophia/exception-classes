1c9f6efe4f1ceb1d28cacb80eab7f3314cc354a0
AleehSophia <alessia.casado@gmail.com>
2022-09-06 10:45:38 -0300

    :sparkles:feat: learning how to use finally block

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,17 +1,39 @@
 package app;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+//        code for class 173
+        File file = new File("C:\\temp\\in.txt");
+        Scanner sc = null;
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        }
+        catch (IOException e) {
+            System.out.println("Error opening file: " + e.getMessage());
+        }
+        finally {
+            if (sc != null) {
+                sc.close();
+            }
+        }
+
+
+        /* code for class 172
         method1();
-
         System.out.println("End of program");
-
+         */
     }
 
+    /* code for class 172
     public static void method1() {
         System.out.println("***METHOD1 START***");
 
@@ -43,5 +65,5 @@ public class Main {
         sc.close();
 
         System.out.println("***METHOD2 END***");
-    }
+    } */
 }
